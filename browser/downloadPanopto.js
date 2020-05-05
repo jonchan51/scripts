b7e77b4ce758d7056a8b8978e4cbe5092c4bbdc8
jonchan51 <32188705+jonchan51@users.noreply.github.com>
2020-05-05 21:02:00 +0800

    Update lecture userscript

--- browser/downloadPanopto.js
+++ browser/downloadPanopto.js
@@ -4,29 +4,25 @@
 // @version      1.0
 // @description  Reveals and adds download link to hidden download button on Panopto
 // @author       jonchan51
-// @match        https://nuscast.ap.panopto.com/Panopto/Pages/Viewer.aspx?*
+// @match        https://*.ap.panopto.com/Panopto/Pages/Viewer.aspx?*
 // @grant        none
 // ==/UserScript==
-
-// Tested with TamperMonkey on FireFox
 
 (function() {
     'use strict';
 
     let downloadButton = document.getElementById("podcastDownload");
 
-    // AJAX makes original downloadButton hidden,
+    // ajax makes original downloadButton hidden,
     // so create new node instead and differentiate from original
     let newDownloadButton = downloadButton.cloneNode(true);
     newDownloadButton.setAttribute("id", "newDownloadButton");
 
     // remove ?id= from search params
     let id = window.location.search.substring(4);
-
-    let downloadUrl = "https://nuscast.ap.panopto.com/Panopto/Podcast/Social/" + id + ".mp4";
-
+    let host = window.location.host;
+    let downloadUrl = "https://" + host + "/Panopto/Podcast/Social/" + id + ".mp4";
     newDownloadButton.addEventListener("click", () => window.open(downloadUrl, "_blank"));
     newDownloadButton.style.visibility = "visible";
-
     downloadButton.parentElement.prepend(newDownloadButton);
 })();
